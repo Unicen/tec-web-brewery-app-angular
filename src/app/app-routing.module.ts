65870b8f715c41d7486fd1ba4c1ef26fd25bf410
Ignacio Jonas <ignaciojonas@gmail.com>
2021-11-01 14:32:27 -0300

    Configure Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BeersComponent } from './beers/beers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo: 'beers',pathMatch: 'full' },
+  { path: 'beers', component: BeersComponent },
+  { path: 'about', component: AboutUsComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
+import { BeersComponent } from './beers/beers.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeerListComponent
+    BeerListComponent,
+    BeersComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
