20b7da6da30c495aa74dacad4f0df8a6dcb1c721
Ignacio Jonas <ignaciojonas@gmail.com>
2021-11-01 14:29:46 -0300

    Create BeerComponent

---
+++ src/app/beers/beers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'beers',
+  templateUrl: './beers.component.html',
+  styleUrls: ['./beers.component.scss']
+})
+export class BeersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
